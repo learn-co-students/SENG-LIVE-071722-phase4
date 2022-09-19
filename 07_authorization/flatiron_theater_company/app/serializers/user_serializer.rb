278c2058a3a23c02bf3b228ff01e250af72762a0
aisayo <aisayo@icloud.com>
2022-09-19 13:32:05 -0400

    lecture code

--- 07_authorization/flatiron_theater_company/app/serializers/user_serializer.rb
+++
@@ -1,5 +0,0 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :username, :id
-  has_many :tickets
-  has_many :productions
-end
--- 08_deploying/flatiron_theater_company/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-    has_many :tickets
-    has_many :productions, through: :tickets
-
-    has_secure_password
-end
