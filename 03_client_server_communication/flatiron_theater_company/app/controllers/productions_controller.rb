48a7ab7373a93a4f8e00942ef623a9b99fad8b26
ixnp <enprocopios@gmail.com>
2022-09-21 09:33:12 -0700

    removed record not found

--- 03_client_server_communication/flatiron_theater_company/app/controllers/productions_controller.rb
+++ 03_client_server_communication/flatiron_theater_company/app/controllers/productions_controller.rb
@@ -7,8 +7,6 @@ class ProductionsController < ApplicationController
     def show
         production = Production.find(params[:id])
         render json: production, status: :ok
-        rescue ActiveRecord::RecordNotFound => error
-            render json: {message: error.message}
     end
 
     def create
@@ -20,16 +18,12 @@ class ProductionsController < ApplicationController
         production = Production.find(params[:id])
         production.update(production_params)
         render json: production, status: :accepted
-        rescue ActiveRecord::RecordNotFound => error
-            render json: {message: error.message}
     end
 
     def destroy
         production = Production.find(params[:id])
         production.destroy
         head :no_content
-        rescue ActiveRecord::RecordNotFound => error
-            render json: {message: error.message}
     end
 
     private
